50d1c063f88790c49866a8fc135555ba8f18d516
Lars Schweighauser <larz258@gmail.com>
2013-05-03 00:00:26 -0700

    v

--- switch.py
+++ switch.py
@@ -9,24 +9,30 @@ class Switch():
         if self.x == self.goal:       #checks the case 
             return True;              #against the goal
         return False;                 #returns False if the check fails
+def build(switchname, switchvalue):
+    switchname = Switch(switchvalue);
+    return switchname.case;
 
 if __name__ == "__main__":            #Doesn't execute this code when imported.
-    MySwitch = Switch(54);            #creates a Switch object 
-    case = MySwitch.case;             #creates a case keyword to minimize
+    case = build("MySwitch", 54);
+    #MySwitch = Switch(54);            #creates a Switch object 
+    #case = MySwitch.case;             #creates a case keyword to minimize
                                       #the amount of typying needed later.
     while True:                       #Creates an infinite loop so that you can break the cases. 
         if case(2123):                #checks the case
             print("That's not 54.");  #does some code
             break;                    #breaks the loop ending the check
-      if case(54):
-          print ("That's the one.");
+        if case(54):
+            print ("That's the one.");
                                       #this does not break, allowing for multiple cases to be activated.
-      if case("Hello"):
-          print ("I'm looking for a number.");
-          break;
-      if not case(None):                            #The default case checks against a value
-          print ("This is the default value you."); #that will always return False. The 'not' makes
-          break;                                    #that False become True.
-      break;                                        #This break is incase no other cases activate. 
-                                                    #Although the default should always activate, that is only if
-                                                    #someone bothers to write a default. 
+        if case("Hello"):
+            print ("I'm looking for a number.");
+            break;
+        if not case(None):                            #The default case checks against a value
+            print ("This is the default value you."); #that will always return False. The 'not' makes
+            break;                                    #that False become True.
+        break;                                        #This break is incase no other cases activate. 
+                                                      #Although the default should always activate, that is only if
+                                                      #someone bothers to write a default. 
+
+
