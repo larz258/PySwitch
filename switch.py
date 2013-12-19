b9f00fe46ebe940c7bad21a7a87ad59e2cabf31f
Lars Schweighauser <larz258@gmail.com>
2013-12-19 15:19:19 -0800

    v
    This is a generic message created by a script. Talk about lazy.

--- switch.py
+++ switch.py
@@ -12,35 +12,37 @@ class Switch():
             goal = False;             #because default uses None
         self.goal = goal; 
 
-    def case(self, x):
+    def Case(self, x):
         self.x = x;
         if self.x == self.goal:       #checks the case 
             return True;              #against the goal
-        return False;                 #returns False if the check fails
-def build(switchvalue):
-    switch = Switch(switchvalue);
-    return switch, switch.case;
+        return False;
+                     #returns False if the check fails
+    def Default():
+        return True;
+      
+def switch(switchvalue):
+    global case, default;
+    current_switch = Switch(switchvalue);
+    case = current_switch.Case;
+    default = current_switch.Default;
+    return True;
 
-if __name__ == "__main__":            #Doesn't execute this code when imported.
-    myswitch, case = build(54);
-    #MySwitch = Switch(54);            #creates a Switch object 
-    #case = MySwitch.case;             #creates a case keyword to minimize
-                                      #the amount of typying needed later.
-    while True:                       #Creates an infinite loop so that you can break the cases. 
-        if case(2123):                #checks the case
-            print("That's not 54.");  #does some code
-            break;                    #breaks the loop ending the check
+if __name__ == "__main__":
+
+    while switch(54): 
+        if case(2123):
+            print("That's not 54.");
+            break;
         if case(54):
             print ("That's the one.");
-                                      #this does not break, allowing for multiple cases to be activated.
+
         if case("Hello"):
             print ("I'm looking for a number.");
             break;
-        if not case(None):                            #The default case checks against a value
-            print ("This is the default value."); #that will always return False. The 'not' makes
-            break;                                    #that False become True.
-        break;                                        #This break is incase no other cases activate. 
-                                                      #Although the default should always activate, that is only if
-                                                      #someone bothers to write a default. 
+        if default:
+            print ("This is the default value.");
+            break;
+        break; 
 
 
