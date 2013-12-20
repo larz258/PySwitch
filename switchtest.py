b410ab9be76152a843245e55f754ecce9a2aabdc
Lars Schweighauser <larz258@gmail.com>
2013-12-20 03:55:35 -0800

    v
    This is a generic message created by a script. Talk about lazy.

--- switchtest.py
+++ switchtest.py
@@ -2,14 +2,14 @@ from PySwitch import switch
 
 while switch(54):                      # sets the value to check against and creates the case and the default functions. 
     if case(2123):
-        print("That's not 54.");       
-        break;
+        print("That's not 54.") 
+        break
     if case(54):
-        print ("That's the one.");
+        print ("That's the one.")
                                        # no break here allows for the default to execute too.
     if case("Hello"):
-        print ("I'm looking for a number.");
-        break;
+        print ("I'm looking for a number.")
+        break
     if default:
-        print ("This is the default value.");
-        break;
+        print ("This is the default value.")
+        break
