6faa1a312d3ff4d03642b627016a901829791c07
Lars Schweighauser <larz258@gmail.com>
2013-12-21 00:13:36 -0800

    v
    This is a generic message created by a script. Talk about lazy.

--- pyswitch.py
+++ pyswitch.py
@@ -1,10 +1,3 @@
-'''PySwitch.py is licensed under the GLPv3
-A copy should have been included with it (LICENSE),
-however if one was not provided you can read the full lincense at
-http://opensource.org/licenses/gpl-3.0.html
-
-copyright Lars Schweighauser, 2013
-'''
 import builtins
 class Switch():
     def __init__(self, goal):                   # goal is the value to be checked against.
