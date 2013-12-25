afc3db03f61b67b65f5e03c8b9be0ac700ecaa64
Lars Schweighauser <larz258@gmail.com>
2013-12-25 00:21:41 -0800

    v
    This is a generic message created by a script. Talk about lazy.

--- pyswitch.py
+++ pyswitch.py
@@ -3,13 +3,23 @@ class Switch():
     def __init__(self, goal):                   # goal is the value to be checked against.
         self.goal = goal 
 
-    def Case(self, to_check):                   # case checks it's argument against the goal.
-        if to_check == self.goal: 
-            return True
-        return False
+    def Case(self, to_check, code, set_state):                   # case checks it's argument against the goal.
+        if state:
+            if to_check == self.goal:
+                if set_state == 0:
+                    return exec(code)
+                if set_state == 1:
+                    builtins.state = False
+                    return exec(code)
 
-    def Default():                              # takes no arguments and always returns true. Because the default case
-        return True                             # should always execute if no other case has.
+    def Default(self, code, set_state):
+        if state:
+            if set_state == 0:
+                return exec(code)
+
+            if set_state == 1:
+                builtins.state = False
+                return exec(code)
 
 def switch(switch_target):                       
     try: del current_switch                     # deletes any already existing switch object.
@@ -17,5 +27,5 @@ def switch(switch_target):
     current_switch = Switch(switch_target)      # creates a Switch object with switch_target as the goal.
     builtins.case = current_switch.Case         # adds the case function to builtins. 
     builtins.default = current_switch.Default   # adds the default function to builtins.
-    return True                                 # returning true creates a quick loop when you write 
+    builtins.state = True                             
                                                 # while switch(arg):
--- switchtest.py
+++ switchtest.py
@@ -1,15 +1,6 @@
 from pyswitch import switch
-
-while switch(54):                      # sets the value to check against and creates the case and the default functions. 
-    if case(2123):
-        print("That's not 54.") 
-        break
-    if case(54):
-        print ("That's the one.")
-                                       # no break here allows for the default to execute too.
-    if case("Hello"):
-        print ("I'm looking for a number.")
-        break
-    if default:
-        print ("This is the default value.")
-        break
+switch(54)
+case(2123, 'print("That\'s not 54.")', 1)
+case(54, 'print ("That\'s the one.")', 0)
+case("Hello", 'print ("I\'m looking for a number")', 1)
+default('print("This is the default value")', 1)
