b3d3c09a9612642ec6ef18e81e4f9665aff1a279
Lars Schweighauser <larz258@gmail.com>
2013-12-25 00:26:59 -0800

    v
    This is a generic message created by a script. Talk about lazy.

--- pyswitch.py
+++ pyswitch.py
@@ -1,9 +1,9 @@
 import builtins
 class Switch():
-    def __init__(self, goal):                   # goal is the value to be checked against.
+    def __init__(self, goal):                   
         self.goal = goal 
 
-    def Case(self, to_check, code, set_state):                   # case checks it's argument against the goal.
+    def Case(self, to_check, code, set_state=1):
         if state:
             if to_check == self.goal:
                 if set_state == 0:
@@ -12,20 +12,15 @@ class Switch():
                     builtins.state = False
                     return exec(code)
 
-    def Default(self, code, set_state):
+    def Default(self, code):
         if state:
-            if set_state == 0:
-                return exec(code)
-
-            if set_state == 1:
-                builtins.state = False
-                return exec(code)
+            return exec(code)
 
 def switch(switch_target):                       
-    try: del current_switch                     # deletes any already existing switch object.
+    try: del current_switch                    
     except NameError: pass
-    current_switch = Switch(switch_target)      # creates a Switch object with switch_target as the goal.
-    builtins.case = current_switch.Case         # adds the case function to builtins. 
-    builtins.default = current_switch.Default   # adds the default function to builtins.
+    current_switch = Switch(switch_target)
+    builtins.case = current_switch.Case
+    builtins.default = current_switch.Default
     builtins.state = True                             
-                                                # while switch(arg):
+
--- switchtest.py
+++ switchtest.py
@@ -1,6 +1,6 @@
 from pyswitch import switch
 switch(54)
-case(2123, 'print("That\'s not 54.")', 1)
+case(2123, 'print("That\'s not 54.")')
 case(54, 'print ("That\'s the one.")', 0)
-case("Hello", 'print ("I\'m looking for a number")', 1)
-default('print("This is the default value")', 1)
+case("Hello", 'print ("I\'m looking for a number")')
+default('print("This is the default value")')
